804c68c62ecfe36857cbd5c1b01e76abbc75fc18
anast-ananko <anast.ananko@gmail.com>
2022-12-04 13:24:24 +0200

    feat: add apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '4654b2068797429a88561ee29a5a890e', // получите свой ключ https://newsapi.org/
         });
     }
 }
