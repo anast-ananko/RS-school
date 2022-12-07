2c874da0aa4049f8610871e228b9124c34d124b0
anast-ananko <anast.ananko@gmail.com>
2022-12-07 15:51:13 +0200

    feat: migrate appView.js to AppView.ts

--- migration-newip-to-ts/src/components/view/appView.ts
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -1,22 +1,25 @@
 import News from './news/news';
 import Sources from './sources/sources';
-import { INewsData, ISourcesData } from '../interfaces/interfaces';
+import { INews } from '../interfaces/interfaces';
+
+export type NewsData = Pick<INews, 'status' | 'totalResults' | 'articles'>;
+export type SourcesData = Pick<INews, 'status' | 'sources'>;
 
 export class AppView {
-    private news: News;
-    private sources: Sources;
+    private readonly news: News;
+    private readonly sources: Sources;
 
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    public drawNews(data: INewsData): void {
+    public drawNews(data: NewsData): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    public drawSources(data: ISourcesData): void {
+    public drawSources(data: SourcesData): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
