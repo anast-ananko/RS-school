cefa00f2cdd2470d9832a9c281b86e7bb8acabae
anast-ananko <anast.ananko@gmail.com>
2022-12-10 14:36:08 +0200

    feat: change url to work on gh-pages

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', {
             apiKey: '4654b2068797429a88561ee29a5a890e', // получите свой ключ https://newsapi.org/
         });
     }
