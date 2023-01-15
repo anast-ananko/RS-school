24ae368eb1a5861a399a48f64aea6046dbd0589c
anast-ananko <anast.ananko@gmail.com>
2023-01-15 19:13:38 +0200

    feat: add api

---
+++ async-race/src/services/apiGarage.ts
@@ -0,0 +1,74 @@
+import { _apiBase } from "./apiBase";
+import { ICar } from "../interfaces/car";
+import { garageCars } from "../types/garageCars";
+
+const _apiGarage = `${_apiBase}/garage`;
+
+export async function getCars(page: number, limit = 7): Promise<garageCars> {
+  const responce: Response = await fetch(
+    `${_apiGarage}?_page=${page}&_limit=${limit}`
+  );
+
+  const cars: Promise<ICar[]> = await responce.json();
+  const count: string | null = responce.headers.get("X-Total-Count");
+
+  return { cars, count };
+}
+
+export async function getCar(id: number): Promise<ICar> {
+  const responce: Response = await fetch(`${_apiGarage}/${id}`);
+  const car: Promise<ICar> = await responce.json();
+
+  return car;
+}
+
+export async function createCar(
+  body: Pick<ICar, "name" | "color">
+): Promise<ICar> {
+  const responce: Response = await fetch(_apiGarage, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const car: Promise<ICar> = await responce.json();
+
+  return car;
+}
+
+export async function deleteCar(id: number): Promise<ICar> {
+  const responce: Response = await fetch(`${_apiGarage}/${id}`, {
+    method: "DELETE",
+  });
+  const car: Promise<ICar> = await responce.json();
+
+  return car;
+}
+
+export async function updateCar(
+  id: number,
+  body: Pick<ICar, "name" | "color">
+): Promise<ICar> {
+  const responce: Response = await fetch(`${_apiGarage}/${id}`, {
+    method: "PUT",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const car: Promise<ICar> = await responce.json();
+
+  return car;
+}
+
+// const updateGarage = async () => {
+//   const { cars, count } = await getCars(1);
+//   console.log(cars);
+//   console.log(count);
+// };
+
+// const ge = async () => {
+//   const car = await getCar(1);
+//   console.log(car);
+// };
